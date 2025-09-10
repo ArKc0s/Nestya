34f31106e7a950a035f945a242c33fe99c167e8d
Léo Wadin <46723431+ArKc0s@users.noreply.github.com>
2025-09-10 15:26:14 +0200

    chore: update application configuration and add dotenv dependency

--- backend/src/main/java/fr/nestya/NestyaBackendApplication.java
+++ backend/src/main/java/fr/nestya/NestyaBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NestyaBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NestyaBackendApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(NestyaBackendApplication.class, args);
 	}
 
 }
