611d66ebfea36792e25306b68f7e4b74a2e7dadb
Léo Wadin <46723431+ArKc0s@users.noreply.github.com>
2025-09-17 09:13:54 +0200

    chore: init react native project

---
+++ frontend/src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+    return (
+        <View className="flex-1 items-center justify-center bg-zinc-100">
+            <Text className="text-xl font-semibold text-zinc-900">
+                Expo 54 + Router + NativeWind OK
+            </Text>
+        </View>
+    );
+}
